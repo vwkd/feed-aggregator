e93e9d7e5a67bc3647f200028b5f12c1d7ec8d9e
vwkd <33468089+vwkd@users.noreply.github.com>
2025-06-04 01:46:08 +0200

    src: make prefix readonly

--- tests/add_expire_test.ts
+++ tests/add_expire_test.ts
@@ -2,35 +2,35 @@ import { assertEquals } from "@std/assert";
 import { delay } from "@std/async";
 import { createFeedAggregator } from "../src/main.ts";
 
-const DELAY_MS = 500;
-const PATH = "tests/add_expire.db";
-const PREFIX = ["my", "example", "feed"];
+const DELAY_MS = 500 as const;
+const PATH = "tests/add_expire.db" as const;
+const PREFIX = ["my", "example", "feed"] as const;
 
-const VERSION = "https://jsonfeed.org/version/1.1";
+const VERSION = "https://jsonfeed.org/version/1.1" as const;
 const INFO = {
   title: "My Example Feed",
   home_page_url: "https://example.org",
   feed_url: "https://example.org/feed.json",
-};
+} as const;
 
 const ITEM1 = {
   id: "1",
   content_html: "<p>Hello, world!</p>",
   url: "https://example.org/initial-post",
-};
+} as const;
 
 const ITEM2 = {
   id: "2",
   content_text: "This is a second item.",
   url: "https://example.org/second-item",
-};
+} as const;
 
 const ITEM3 = {
   id: "3",
   content_html: "<p>This is a third item.</p>",
   content_text: "This is a third item.",
   url: "https://example.org/third-item",
-};
+} as const;
 
 Deno.test("add", async () => {
   const expected = JSON.stringify({
